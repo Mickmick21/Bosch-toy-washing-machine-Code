7461c7dacc5df6c3776ecc987898e7cc971f4074
Mickmick2012 <michelangelochaume@gmail.com>
2023-02-14 11:30:00 +0100

    Add files via upload

---
+++ washerfunctions.h
@@ -0,0 +1,277 @@
+#if !defined(END)
+#error "END Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B0)
+#error "NOTE_B0 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C1)
+#error "NOTE_C1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS1)
+#error "NOTE_CS1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D1)
+#error "NOTE_D1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS1)
+#error "NOTE_DS1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E1)
+#error "NOTE_E1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F1)
+#error "NOTE_F1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E1)
+#error "NOTE_E1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F1)
+#error "NOTE_F1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_FS1)
+#error "NOTE_FS1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_G1)
+#error "NOTE_G1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_A1)
+#error "NOTE_A1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_AS1)
+#error "NOTE_AS1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B1)
+#error "NOTE_B1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C2)
+#error "NOTE_C2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS2)
+#error "NOTE_CS2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_GS1)
+#error "NOTE_GS1 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D2)
+#error "NOTE_D2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS2)
+#error "NOTE_DS2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E2)
+#error "NOTE_E2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F2)
+#error "NOTE_F2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_FS2)
+#error "NOTE_FS2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_G2)
+#error "NOTE_G2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_GS2)
+#error "NOTE_GS2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_A2)
+#error "NOTE_A2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_AS2)
+#error "NOTE_AS2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B2)
+#error "NOTE_B2 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C3)
+#error "NOTE_C3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS3)
+#error "NOTE_CS3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D3)
+#error "NOTE_D3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS3)
+#error "NOTE_DS3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E3)
+#error "NOTE_E3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F3)
+#error "NOTE_F3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_FS3)
+#error "NOTE_FS3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_G3)
+#error "NOTE_G3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_GS3)
+#error "NOTE_GS3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_A3)
+#error "NOTE_A3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_AS3)
+#error "NOTE_AS3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B3)
+#error "NOTE_B3 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C4)
+#error "NOTE_C4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS4)
+#error "NOTE_C4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D4)
+#error "NOTE_C4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS4)
+#error "NOTE_C4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E4)
+#error "NOTE_C4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F4)
+#error "NOTE_F4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_FS4)
+#error "NOTE_FS4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_G4)
+#error "NOTE_G4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_GS4)
+#error "NOTE_GS4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_A4)
+#error "NOTE_A4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_AS4)
+#error "NOTE_AS4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B4)
+#error "NOTE_B4 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C5)
+#error "NOTE_C5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS5)
+#error "NOTE_CS5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D5)
+#error "NOTE_D5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS5)
+#error "NOTE_DS5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E5)
+#error "NOTE_E5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F5)
+#error "NOTE_D5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_FS5)
+#error "NOTE_FS5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_G5)
+#error "NOTE_G5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_GS5)
+#error "NOTE_GS5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_A5)
+#error "NOTE_D5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_AS5)
+#error "NOTE_AS5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B5)
+#error "NOTE_B5 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C6)
+#error "NOTE_C6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS6)
+#error "NOTE_CS6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D6)
+#error "NOTE_D6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS6)
+#error "NOTE_DS6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E6)
+#error "NOTE_E6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F6)
+#error "NOTE_C6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_FS6)
+#error "NOTE_C6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_G6)
+#error "NOTE_G6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_GS6)
+#error "NOTE_GS6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_A6)
+#error "NOTE_A6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_AS6)
+#error "NOTE_AS6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B6)
+#error "NOTE_B6 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C7)
+#error "NOTE_C7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS7)
+#error "NOTE_CS7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D7)
+#error "NOTE_D7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS7)
+#error "NOTE_DS7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_E7)
+#error "NOTE_E7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_F7)
+#error "NOTE_F7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_FS7)
+#error "NOTE_FS7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_G7)
+#error "NOTE_G7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_GS7)
+#error "NOTE_GS7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_A7)
+#error "NOTE_A7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_AS7)
+#error "NOTE_A7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_B7)
+#error "NOTE_B7 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_C8)
+#error "NOTE_C8 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_CS8)
+#error "NOTE_CS8 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_D8)
+#error "NOTE_D8 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+#if !defined(NOTE_DS8)
+#error "NOTE_DS8 Was not defined, file "pitches.h" is probably mising or corupted"
+#endif
+
